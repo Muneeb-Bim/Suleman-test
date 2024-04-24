f4935068467edd807d229a85fee741c39c8bdd26
Muneeb-Bim <167433480+Muneeb-Bim@users.noreply.github.com>
2024-04-24 12:48:03 +0500

    Delete .github/workflows/db.js

--- .github/workflows/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-module.exports = new Sequelize("yelpp" , "postgres" , "Suleman@123", {
-    host: "localhost",
-    dialect: "postgres",
-    // operatorsAliases: false,
-    pool: 5432,
-
-}) 
